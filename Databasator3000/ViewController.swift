1483617c041ab15f8af448b23243887d2b5cfc11
Jiri Bucek <bigboocha@gmail.com>
2018-11-01 18:32:22 +0430

    Initial Commit

---
+++ Databasator3000/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Databasator3000
+//
+//  Created by Boocha on 01.11.18.
+//  Copyright © 2018 Boocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
